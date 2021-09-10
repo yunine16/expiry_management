e7611ff6c859c4be381a4f9a130aad655c465c76
mono <83483550+grothendieckmono@users.noreply.github.com>
2021-09-10 23:09:02 +0900

    Update seed.py
    改行加える等の調整しました。

--- app/src/seeds/seed.py
+++ app/src/seeds/seed.py
@@ -1,6 +1,7 @@
 from flask_seeder import Seeder, Faker, generator
 from models.user import User
 from models.food import Food
+
 # All seeders inherit from Seeder
 class DemoSeeder(Seeder):
 
@@ -9,11 +10,12 @@ class DemoSeeder(Seeder):
 
         jack_users = [
             User(mailAddress="mochi@jack.com"),
-            User(mailAddress="mono@jack.com" ),
-            User(mailAddress="nine@jack.com" ),
-            User(mailAddress="zuo@jack.com"  ),
-            User(mailAddress="wato@jack.com" )
+            User(mailAddress="mono@jack.com"),
+            User(mailAddress="nine@jack.com"),
+            User(mailAddress="zuo@jack.com"),
+            User(mailAddress="wato@jack.com")
         ]
+        
         for jack_user in jack_users:
             print("Adding user: %s" % jack_user)
             self.db.session.add(jack_user)
@@ -25,18 +27,20 @@ class DemoSeeder(Seeder):
             "mailAddress": generator.Email(),
           }
         )
+        
         # Create 5 fake users 
         for user in user_faker.create(5):
             print("Adding user: %s" % user)
             self.db.session.add(user)
 
         sample_foods = [
-            Food(name="大根"      , expiry_date="2021-10-10", user_id=1),
-            Food(name="人参"      , expiry_date="2021-10-10", user_id=1),
-            Food(name="秋刀魚"    , expiry_date="2021-10-10", user_id=2),
+            Food(name="大根", expiry_date="2021-10-10", user_id=1),
+            Food(name="人参", expiry_date="2021-10-10", user_id=1),
+            Food(name="秋刀魚", expiry_date="2021-10-10", user_id=2),
             Food(name="こんにゃく", expiry_date="2021-10-10", user_id=2),
-            Food(name="ぶどう"    , expiry_date="2021-10-10", user_id=3)
+            Food(name="ぶどう", expiry_date="2021-10-10", user_id=3)
         ]
+        
         for sample_food in sample_foods:
             print("Adding food: %s" % sample_food)
             self.db.session.add(sample_food)
@@ -50,6 +54,7 @@ class DemoSeeder(Seeder):
             "user_id":generator.Integer()
           }
         )
+        
         # Create 5 fake foods
         for food in food_faker.create(5):
             print("Adding food: %s" % food)
