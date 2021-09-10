281eada3b58b44bf1f83ffe367fbb2f2288f0334
morita-koki <morita82007koki@gmail.com>
2021-09-10 17:00:25 +0900

    サンプルデータを生成するseedの作成

--- app/src/models/user.py
+++ app/src/models/user.py
@@ -7,6 +7,12 @@ class User(db.Model):
     __tablename__ = 'Users'
 
     id = db.Column(db.Integer, primary_key=True)
-    mailAddress = db.Column(db.String(255))
+    mailAddress = db.Column(db.String(255), nullable=False, primary_key=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    def __init__(self, mailAddress=None):
+        self.mailAddress = mailAddress
+
+    def __str__(self):
+        return "mailAddress = %s" % self.mailAddress
---
+++ app/src/seeds/seed.py
@@ -0,0 +1,56 @@
+from flask_seeder import Seeder, Faker, generator
+from models.user import User
+from models.food import Food
+# All seeders inherit from Seeder
+class DemoSeeder(Seeder):
+
+    # run() will be called by Flask-Seeder
+    def run(self):
+
+        jack_users = [
+            User(mailAddress="mochi@jack.com"),
+            User(mailAddress="mono@jack.com" ),
+            User(mailAddress="nine@jack.com" ),
+            User(mailAddress="zuo@jack.com"  ),
+            User(mailAddress="wato@jack.com" )
+        ]
+        for jack_user in jack_users:
+            print("Adding user: %s" % jack_user)
+            self.db.session.add(jack_user)
+
+        # Create a new Faker and tell it how to create User objects
+        user_faker = Faker(
+          cls=User,
+          init={
+            "mailAddress": generator.Email(),
+          }
+        )
+        # Create 5 fake users 
+        for user in user_faker.create(5):
+            print("Adding user: %s" % user)
+            self.db.session.add(user)
+
+        sample_foods = [
+            Food(name="大根"      , expiry_date="2021-10-10", user_id=1),
+            Food(name="人参"      , expiry_date="2021-10-10", user_id=1),
+            Food(name="秋刀魚"    , expiry_date="2021-10-10", user_id=2),
+            Food(name="こんにゃく", expiry_date="2021-10-10", user_id=2),
+            Food(name="ぶどう"    , expiry_date="2021-10-10", user_id=3)
+        ]
+        for sample_food in sample_foods:
+            print("Adding food: %s" % sample_food)
+            self.db.session.add(sample_food)
+
+        # Create a new Faker and tell it how to create Food objects
+        food_faker = Faker(
+          cls=Food,
+          init={
+            "name": generator.Name(),
+            "expiry_date":"2021-10-10",
+            "user_id":generator.Integer()
+          }
+        )
+        # Create 5 fake foods
+        for food in food_faker.create(5):
+            print("Adding food: %s" % food)
+            self.db.session.add(food)
