b471d682c5b1b9878f10c0c949ff26956252cc3d
morita-koki <morita82007koki@gmail.com>
2021-09-12 01:46:22 +0900

    サンプルデータ作成

--- app/src/models/user.py
+++ app/src/models/user.py
@@ -6,7 +6,7 @@ import uuid
 class User(db.Model):
     __tablename__ = 'Users'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     mailAddress = db.Column(db.String(255), nullable=False, primary_key=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
--- app/src/seeds/seed.py
+++ app/src/seeds/seed.py
@@ -1,4 +1,4 @@
-from flask_seeder import Seeder, Faker, generator
+from flask_seeder import Seeder
 from models.user import User
 from models.food import Food
 
@@ -20,19 +20,6 @@ class DemoSeeder(Seeder):
             print("Adding user: %s" % jack_user)
             self.db.session.add(jack_user)
 
-        # Create a new Faker and tell it how to create User objects
-        user_faker = Faker(
-          cls=User,
-          init={
-            "mailAddress": generator.Email(),
-          }
-        )
-        
-        # Create 5 fake users 
-        for user in user_faker.create(5):
-            print("Adding user: %s" % user)
-            self.db.session.add(user)
-
         sample_foods = [
             Food(name="大根", expiry_date="2021-10-10", user_id=1),
             Food(name="人参", expiry_date="2021-10-10", user_id=1),
@@ -44,18 +31,4 @@ class DemoSeeder(Seeder):
         for sample_food in sample_foods:
             print("Adding food: %s" % sample_food)
             self.db.session.add(sample_food)
-
-        # Create a new Faker and tell it how to create Food objects
-        food_faker = Faker(
-          cls=Food,
-          init={
-            "name": generator.Name(),
-            "expiry_date":"2021-10-10",
-            "user_id":generator.Integer()
-          }
-        )
         
-        # Create 5 fake foods
-        for food in food_faker.create(5):
-            print("Adding food: %s" % food)
-            self.db.session.add(food)
