8ddc103099c9810647904b3d3418554bc1fdb273
ずおちゃん <84133517+zuochan@users.noreply.github.com>
2021-09-21 17:57:24 +0900

    ユーザーの追加

--- app/src/apis/user.py
+++ app/src/apis/user.py
@@ -9,7 +9,13 @@ class User(Resource):
     def get(self):
         return
 
-    def post(self):
+    def post(self, user_name, mailAddress):
+        import time
+        date=datetime.date.today()
+        user_a=Users(name=user_name, mailAdress=mailAdress, created_at=date, updated_at=date)
+        db.session.add(user_a)
+        db.session.flush()
+        db.session.commit()
         return
 
     def delete(self):
