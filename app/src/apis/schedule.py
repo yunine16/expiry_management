66fc3989695aad2801739be2a89648508be2e72a
mono <sirasu.22360679@icloud.com>
2021-09-29 21:19:22 +0900

    schedule.pyの修正（賞味期限の近い食品の取得）

--- app/src/apis/schedule.py
+++ app/src/apis/schedule.py
@@ -1,13 +1,9 @@
 from datetime import datetime
+from flask_restful import Resource, reqparse, abort
 from database import db
 from models import User, Food
-
-
-class a:
-    def get(self,mailadress):
-        today=datetime.date.today()+datetime.timedelta(days=7)
-        id=db.session.query(Users).filter(Users.mailAdress==mailAdress)
-        food=db.session.query(Foods).filter(Foods.user_id==user_id,Foods.expirly_date<=today).all()
-        return food
-
-        
+class FoodCheckApi(Resource):
+    def get(self,user_id):
+        day=datetime.date.today()+datetime.timedelta(days=7)
+        expired_food=db.session.query(Food).filter(Food.user_id==user_id,Food.expirly_date<=day).all()
+        return expired_food
